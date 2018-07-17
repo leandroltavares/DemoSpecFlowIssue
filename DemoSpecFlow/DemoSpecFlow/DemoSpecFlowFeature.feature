0109783a033636ce84d47eb3f707646a8dceccfc
Leandro Luciani Tavares <leandro.ltavares@gmail.com>
2018-07-17 16:15:48 -0300

    MCV example
    Minimal, Complete, and Verifiable example

---
+++ DemoSpecFlow/DemoSpecFlow/DemoSpecFlowFeature.feature
@@ -0,0 +1,5 @@
+﻿Funcionalidade: DemoSpecFlowFeature
+	In order to show the accent step
+
+Cenario: DemoScenario
+	Dado that I used a step with accents like á or ã and parameter áã
---
+++ DemoSpecFlow/DemoSpecFlow/DemoStepDefinition.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace DemoSpecFlow
+{
+    [Binding]
+    public sealed class DemoStepDefinition
+    {
+        [Given("that I used a step with accents like á or ã and parameter (.*)")]
+        public void SampleAccentStep(string parameter)
+        {
+            Assert.IsTrue(parameter.Contains("á") && parameter.Contains("ã"));
+        }
+    }
+}
